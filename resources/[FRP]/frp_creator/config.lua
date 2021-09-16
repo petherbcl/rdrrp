1b8f581e408ab01901597019cead8b1b7c40613a
Pedro Cardoso <pmcardoso.bcl@gmail.com>
2021-09-16 17:48:28 +0100

    fix coordenadas de chegada de nova personagem
    fix coordenadas de chegada de nova personagem

--- resources/[FRP]/frp_creator/config.lua
+++ resources/[FRP]/frp_creator/config.lua
@@ -2,4 +2,4 @@ Config = {}
 
 Config.EnableCutscene = false  --// ENABLE IF YOU CAN WAGON CUTSCENE
 Config.fadeOutTime = 12000 -- // Waiting time to remove fadeout
-Config.FirstSpawnCoords = vector3(2539.83,-1129.33,50.03)  -- // COORDINATES TO SPAWN PLAYER IF CUTSCENE IS DISABLED
+Config.FirstSpawnCoords = vector3(2712.024,-1449.021,46.253)  -- // COORDINATES TO SPAWN PLAYER IF CUTSCENE IS DISABLED
