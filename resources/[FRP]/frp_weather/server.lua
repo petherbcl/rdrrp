d21303b118fa52f2f93938c7591aba018f3518b5
Pedro Cardoso <pmcardoso.bcl@gmail.com>
2021-09-23 16:34:17 +0100

    fix frp_weather
    fix fr_weather

--- resources/[FRP]/frp_weather/server.lua
+++ resources/[FRP]/frp_weather/server.lua
@@ -1,3 +1,8 @@
+local Tunnel = module("_core", "lib/Tunnel")
+local Proxy = module("_core", "lib/Proxy")
+API = Proxy.getInterface("API")
+cAPI = Tunnel.getInterface("API")
+
 local weatherPerRegion = {
     ["Heartlands"] = {
       --  ["rain"] = 0.1,
